d8b4831615d1ae1c65fd6ee3db6f2e572f8425e8
Kasem32833 <abulkasem32833@gmail.com>
2023-09-14 19:55:51 +0300

    create a react project with tailwindcss and cleanup app.cssand index.css and add code for tailwind cdd

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div className="flex container mx-auto mt-10">
+            <h2 className="text-4xl font-bold">Knowledge cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
