c1772af4fd5d2477fd3e283c57382e2be364a1ed
Kasem32833 <abulkasem32833@gmail.com>
2023-09-14 20:30:34 +0300

    create fake data using chatGpt and creat a blogs.json file and save that data here and load data by useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
